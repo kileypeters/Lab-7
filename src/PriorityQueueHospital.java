1f755d7c684df959eeea9acd5d8fb54c21565a91
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-03-12 23:55:08 -0500

    Completed methods

--- src/PriorityQueueHospital.java
+++ src/PriorityQueueHospital.java
@@ -1,4 +1,54 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class PriorityQueueHospital {
+/**
+ * Class to represent the PriorityQueueHospital. Inherits hospital.
+ * 
+ * @author Kiley
+ *
+ * @param <PatientType>
+ */
+public class PriorityQueueHospital<PatientType extends Comparable<PatientType>> extends Hospital<PatientType> {
 
+	//Queue data structure for patients.
+	private Queue<PatientType> myPriorityQueue = new LinkedList<PatientType>();
+	
+	@Override
+	public void addPatient(PatientType patient) {
+		myPriorityQueue.add(patient);
+		
+	}
+
+	@Override
+	public PatientType nextPatient() {
+		return myPriorityQueue.peek();
+	}
+
+	@Override
+	public PatientType treatNextPatient() {
+		PatientType next = myPriorityQueue.peek();
+		myPriorityQueue.remove();
+		
+		return next;
+	}
+
+	@Override
+	public int numPatients() {
+		
+		return myPriorityQueue.size();
+	}
+
+	@Override
+	public String hospitalType() {
+		
+		return getClass().getName();
+	}
+
+	@Override
+	public String allPatientInfo() {
+		
+		return myPriorityQueue.toString();
+	}
+
+	
 }
