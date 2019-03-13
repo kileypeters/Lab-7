c18759fe154b59f084be43b8b0c6ee5973d01e33
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-03-12 23:54:25 -0500

    Updated

--- Todo.java
+++ Todo.java
@@ -5,25 +5,28 @@ OU ID_______________: 113371387
 OU EMAIL____________: Kiley.m.peters-1@ou.edu
 
 Tasks:
-1. 
-2. 
-3. 
-4.
-5.
+1. Complete SickPerson class.
+2. Complete HealthyPerson class.
+3. Complete StackHospital class.
+4. Complete QueueHospital class.
+5. Complete PriorityQueueHospital class.
+6. Create tests.
 
 
 
 Estimated Time (minutes):
-1.
-2.
-3.
-4.
-5.
+1. 60
+2. 60
+3. 60
+4. 60
+5. 60
+6. 120
 
 Actual Time (minutes):
-1.
-2.
-3.
-4.
-5.
+1. 40
+2. 30
+3. 40
+4. 30
+5. 30
+6. 60
 */}
