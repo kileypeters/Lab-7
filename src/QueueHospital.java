50ca336c1968560a19688de4c78b8f299c258860
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-03-12 23:55:14 -0500

    Completed methods

--- src/QueueHospital.java
+++ src/QueueHospital.java
@@ -1,4 +1,55 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class QueueHospital {
+/**
+ * Class to represent the QueueHospital. Inherits hospital.
+ * 
+ * @author Kiley
+ *
+ * @param <PatientType>
+ */
+public class QueueHospital<PatientType> extends Hospital<PatientType> {
+
+	//Queue data structure for patients.
+	private Queue<PatientType> myQueue = new LinkedList<PatientType>();
+	
+	@Override
+	public void addPatient(PatientType patient) {
+		myQueue.add(patient);
+		
+	}
+
+	@Override
+	public PatientType nextPatient() {
+		
+		return myQueue.peek();
+	}
+
+	@Override
+	public PatientType treatNextPatient() {
+		PatientType next = myQueue.peek();
+		myQueue.remove();
+		
+		return next;
+	}
+
+	@Override
+	public int numPatients() {
+		
+		return myQueue.size();
+	}
+
+	@Override
+	public String hospitalType() {
+		
+		return getClass().getName();
+	}
+
+	@Override
+	public String allPatientInfo() {
+		
+		return myQueue.toString();
+	}
+
 
 }
