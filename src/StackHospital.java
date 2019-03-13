2ec4e9474308a9bc50ba41f428c579301355f56d
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-03-12 23:55:29 -0500

    Completed methods

--- src/StackHospital.java
+++ src/StackHospital.java
@@ -1,4 +1,56 @@
+import java.util.Stack;
 
-public class StackHospital {
+/**
+ * Class representing the StackHospital. Inherits Hospital.
+ * 
+ * @author Kiley
+ *
+ * @param <PatientType>
+ */
+public class StackHospital<PatientType> extends Hospital<PatientType> {
+
+	//Stack data structure for patients.
+	private Stack<PatientType> myStack = new Stack<PatientType>();
+	
+	@Override
+	public void addPatient(PatientType patient) {
+		
+		myStack.push(patient);
+	}
+
+	@Override
+	public PatientType nextPatient() {
+		
+		return myStack.peek();
+	}
+
+	@Override
+	public PatientType treatNextPatient() {
+		PatientType next = myStack.peek();
+		myStack.pop();
+		
+		return next;
+	}
+
+	@Override
+	public int numPatients() {
+		
+		return myStack.size();
+	}
+
+	@Override
+	public String hospitalType() {
+		
+		return getClass().getName();
+	}
+
+	@Override
+	public String allPatientInfo() {
+		
+		
+		return myStack.toString();
+	}
+
+	
 
 }
